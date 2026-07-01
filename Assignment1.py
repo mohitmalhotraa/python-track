327e9f42e2b63c0545d07aa82a22cb40faf423b7
Mohit Malhotra <mohit.malhotra9765z@gmail.com>
2026-07-01 10:25:16 +0530

    Third commit

--- Assignment1.py
+++ Assignment1.py
@@ -1,5 +1,5 @@
 #Question 1
-def emp_bonus(att,perf):
+"""def emp_bonus(att,perf):
     if (att>=75 and perf>=6):
         print("Eligible")
     else:
@@ -53,6 +53,99 @@ def simple_int(P,R,T):
 
 P,R,T = inp_data()
 simple_int(P,R,T)
+
+#Question 5 
+def ager():
+    age=int(input("Enter Age: "))
+
+    return age 
+def tkt_price(age):
+    if(age < 18):
+        TicketPrice = "Rs 100"
+    elif(18 <= age < 60):
+        TicketPrice = "Rs 200"
+    else:
+        TicketPrice = "Rs 150"
+    return TicketPrice
+age = ager()
+print(tkt_price(age))
+
+#Question6 WAP to find the largest among three entered sales figures
+def larger_num(num1,num2,num3):
+    if (num1>num2>num3 or num1>num3>num2):
+        print(num1,"is largest among all")
+    elif (num2>num1>num3 or num2>num3>num1):
+        print(num2,"is largest among all")
+    else:
+        print(num3,"is largest among all")
+larger_num(10,12,50)"""
+
+
+
+# #Question 7 A school wants to calculate grades for students based on marks obtained in five subjects
+# def ip_marks():
+#     for i in range(0,5):
+#         a = a.extend[0](int(input("Marks of subject")))
+        
+#     return Marks
+# def grade_marks(Marks):
+#     avg= sum(Marks)/5
+#     if(avg>90):
+#         print("A grade")
+#     elif(avg>80):
+#         print("B grade")
+#     elif(avg>70):
+#         print("C grade")
+#     elif(avg>60):
+#         print("D grade")
+#     elif(avg>50):
+#         print("E grade")
+#     else:
+#         print("F grade")
+
+# Marks = ip_marks()
+# grade_marks(Marks)
+
+#Question8 3 Maximum login attempt
+# def get_pass():
+    
+#     return password
+
+def login(attempt = 0):
+    
+    while attempt< 3:
+        password = input("Enter Password: ")
+        
+        if (password=="admin"):
+            print("Login Successfully")
+            break
+        
+        else:
+            attempt += 1
+        print("Login Unsuccessful")
+
+
+login()
+
+
+
+
+    
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- ds.py
+++ ds.py
@@ -1,3 +1,13 @@
-a = "Python is Awesome"
-for i in range (0,0 ,-1):
-    print(a)
+def revrese_words(sentence):
+    words = sentence.split()
+    result = []
+    for i in range(len(words) -1,-1,-1):
+        result.append(words[i])
+    return" ".join (result)
+# text ="Python is awesome" 
+print(revrese_words("Python is awesome"))
+
+def repeating(str):
+    
+
+                    
